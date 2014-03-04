e311c128f2c5288f6f2fdab9fca2f9d29a496f91
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-03-04 14:40:15 +0100

    created method interface for classes

--- src/TCPHandler.java
+++ src/TCPHandler.java
@@ -1,4 +1,13 @@
 
 public class TCPHandler{
+    
+    public TCPHandler(int tcpPort){
+        
+    }
+
+    public void sendMessage(String string) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
--- src/WebSocketServer.java
+++ src/WebSocketServer.java
@@ -1,4 +1,7 @@
 
 public class WebSocketServer{
 
+    public WebSocketServer(int websocketPort){
+        
+    }
 }
