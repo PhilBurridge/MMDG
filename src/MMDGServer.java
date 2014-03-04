32dcac6f86fbb765a87c42c82a6d08c917d9502d
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-03-04 14:16:41 +0100

    added class files from UML class diagram. No methods implemeneted

--- src/MMDGServer.java
+++ src/MMDGServer.java
@@ -1,9 +1,38 @@
 public class MMDGServer{
 
     /**
-     * @param args
+     *  This is the HTTP server handler class
      */
+    private HTTPServer httpServer;
     
+    /**
+     * This is the WebSocket handler class
+     */
+    private WebSocketServer webSocketServer;
+    
+    /**
+     * This is the TCP handler class
+     */
+    private TCPHandler tcpHandler;
+    
+    
+    
+    
+   /**
+    * This is a simple constructor
+    */
+    public MMDGServer(){
+        System.out.print("init MMDGServer ... ");
+        httpServer = new HTTPServer();
+        webSocketServer = new WebSocketServer();
+        tcpHandler = new TCPHandler();
+        System.out.println("Done!");
+    }
+    
+    /**
+     * This is just a test method.
+     * @return a string with a greeting message
+     */
     public String greet(){
         return "MMDG: Hellow world!";
     }
--- src/MMDGServerTest.java
+++ src/MMDGServerTest.java
@@ -6,6 +6,7 @@ public class MMDGServerTest{
      */
     public static void main(String[] args) {
        MMDGServer mmdgServer = new MMDGServer();
+       
        System.out.println(mmdgServer.greet());
 
     }
