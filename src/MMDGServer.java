b0e9db205373b521459647db39b7178b17dedda7
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-03-04 12:12:17 +0100

    created eclipse project in git folder. Git ignore bin and eclipse project files

---
+++ src/MMDGServer.java
@@ -0,0 +1,12 @@
+
+public class MMDGServer{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Welcome the MMDG server to the project!
+        System.out.println("MMDG: Hello world!");
+    }
+
+}
