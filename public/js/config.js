e0da41feb5cb28137e24b23cb94aa0c509011843
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-03-15 23:01:31 +0100

    Committing new IP. will fix this

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 (function (exports) {
-    exports.serverIP = "130.236.67.188";
+    exports.serverIP = "83.191.226.199";
     exports.serverWsPort= 1338;
 })(typeof exports === 'undefined' ? this['config']={} : exports);
