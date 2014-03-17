276fa7ef8b1489ff898394a5bf036d2998469b06
Erik Broberg <erik_broberg_cool@hotmail.com>
2014-03-17 10:09:59 +0100

    added TCP stuff to TCP branch

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+(function (exports) {
+    exports.serverIP = "192.168.0.6";
+    exports.serverWsPort= 1338;
+})(typeof exports === 'undefined' ? this['config']={} : exports);
--- src/TCPHandler.java
+++ src/TCPHandler.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.net.*;
 import java.util.Vector;
 
 
@@ -24,3 +26,18 @@ public class TCPHandler extends ConsolePrinter{
         sendMessage(messages);
     }
 }
+
+/*
+String sentence;
+String modifiedSentence;
+BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+Socket clientSocket = new Socket("localhost", 20501);
+DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+sentence = inFromUser.readLine();
+outToServer.writeBytes(sentence + '\n');
+modifiedSentence = inFromServer.readLine();
+System.out.println(modifiedSentence);
+//clientSocket.close();*/
