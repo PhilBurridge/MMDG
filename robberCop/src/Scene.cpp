a7b28e0900a5f2a3b158f7691a091ba487ab7553
Philburridge <burridge@live.se>
2014-04-14 09:44:19 +0200

    Added text

--- robberCop/src/Scene.cpp
+++ robberCop/src/Scene.cpp
@@ -15,12 +15,13 @@ void updatePositions() {
 }
 
 void checkCollisions() {
-    for(std::vector<Player> itCop = player_vec; itCop != player_vec.end(); itCop++) { // jag hade ingen bättre fantasi än 
+    for(std::vector<Player> itCop = player_vec.begin(); itCop != player_vec.end(); itCop++) { // jag hade ingen bättre fantasi än 
         if((*itCop)->isCop()) {
-            for(std::vector<Player> itRob = player_vec; itRob != player_vec.end(); itRob++) {
+            for(std::vector<Player> itRob = player_vec.begin(); itRob != player_vec.end(); itRob++) {
                 if(!(*itRob)->isCop() && 
                     lenght((*isCop)->position - (*isRob)->position) > ((*isCop)->size + (*isRob)->size)) { // oskäer på length
                     // Do somthing when collision happens. KILL THA ROBBBA
+                    std::cout << "collision between player " << std::endl;
                 }
             }
         }
@@ -28,7 +29,7 @@ void checkCollisions() {
 }
 
 void draw() {
-    
+    // Borde bara loopa igenom alla player och rita ut dem mha draw i drawableObject
 }
 
 
