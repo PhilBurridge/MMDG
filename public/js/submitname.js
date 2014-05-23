6e551d10a11ff4be7de80394a8bbaa6e09642ad9
Makkalakka <antal650@student.liu.se>
2014-05-23 11:17:16 +0200

    färdigt popupfönster och fin hemsida

--- public/js/submitname.js
+++ public/js/submitname.js
@@ -23,8 +23,14 @@ Submitname.prototype = {
 			unique = false;
 		}*/
 		
-		if(person == null || person.length > 12 || person.length < 3 || person == "kuk" || person == "penis" || person == "fitta" || person == "slida" || person == "bajs" || person == "fuck" || person == "döda" || person == "neger" || person == "hora" || person == "cp" || person == "jude" || person == "guling" || person == "nigger" || person == "knark" || person == "sex" || person == "knull" || person == "knulla" || person == "samlag" || person == "dick" || person == "slut" || person == "whore" || person == "shit" || person == "nigga" || person == "pussy" || person == "ass" || person == "bitch" || person == "beatch" || person == "piss"){
-			alert("That name already exists or you have typed the wrong amount of letters!");
+		if (person == null || person.length > 12 || person.length < 2 || person == "kuk" || person == "penis" 
+		|| person == "fitta" || person == "slida" || person == "bajs" || person == "fuck" || person == "döda" 
+		|| person == "neger" || person == "hora" || person == "cp" || person == "jude" || person == "guling" 
+		|| person == "nigger" || person == "knark" || person == "sex" || person == "knull" || person == "knulla" 
+		|| person == "samlag" || person == "dick" || person == "slut" || person == "whore" || person == "shit" 
+		|| person == "nigga" || person == "pussy" || person == "ass" || person == "bitch" || person == "beatch" 
+		|| person == "piss" || person == "skit" || person == "död" || person == "snopp" || person == "puta"){
+			alert("That nickname is invalid!");
 			return false;
 		}
 		else{
