fc7c955d97eae992afe315cc361c7877a189bc95
Erik Broberg <erikbroberg90@gmail.com>
2014-06-02 19:44:01 +0200

    test post sync

---
+++ newSGCTapp/src/CMakeFiles/newSGCTapp.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/DrawableSquare.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/DrawableSquare.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/ModelMesh.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/ModelMesh.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/Scene.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/Scene.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/core.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/core.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/main.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/main.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/player.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/player.cpp.o"
+  "/Users/erik/Documents/git/MMDG/newSGCTapp/src/robberCop.cpp" "/Users/erik/Documents/git/MMDG/newSGCTapp/src/CMakeFiles/newSGCTapp.dir/robberCop.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "__APPLE__"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ newSGCTapp/src/CMakeFiles/newSGCTapp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/newSGCTapp.dir/core.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/DrawableSquare.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/main.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/ModelMesh.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/player.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/robberCop.cpp.o"
+  "CMakeFiles/newSGCTapp.dir/Scene.cpp.o"
+  "newSGCTapp.pdb"
+  "newSGCTapp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/newSGCTapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
